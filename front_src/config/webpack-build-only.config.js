d99257fe548453e2a53ee113d3d4335b8d3c7111
Davis-Software <davissoftware6@gmail.com>
2022-10-06 16:39:48 +0200

    added build only mode to webpack

---
+++ front_src/config/webpack-build-only.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mode:"production",
+
+    entry: "./src",
+
+    output: {
+        filename: "main.js",
+        path: __dirname + "/../../static/js/views"
+    },
+
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                exclude: /node_modules$/
+            }
+        ]
+    },
+
+    devtool: "none"
+}
